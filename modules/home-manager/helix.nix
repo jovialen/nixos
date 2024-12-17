b3033dc8cc274ef579361dde64628e07ef81e197
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2024-12-17 17:53:02 +0100

    fix: broken helix config

--- modules/home-manager/helix.nix
+++ modules/home-manager/helix.nix
@@ -11,19 +11,11 @@ in
   config.programs.helix = lib.mkIf cfg.enable {
     enable = true;
     settings = {
-      editor = {
-        line-numer = "relative";
-        lsp.display-messages = true;
-      };
-      keys.normal = {
-        space.space = "file_picker";
-        space.w = ":w";
-        space.q = ":wq";
-        esc = [ "collapse_selection" "keep_primary_selecton" ];
-      };
     };
     ignores = [
       "node_modules"
+      "build"
+      "bin"
       "!.gitignore"
     ];
   };
